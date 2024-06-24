dbe34783d91401b0e1b12842666a2edf3bc5bf02
jinyus <jinyhehe@gmail.com>
2024-06-24 14:07:49 +0900

    feat: csrf token 활성화

--- src/main/java/com/juny/spacestory/global/config/SecurityConfig.java
+++ src/main/java/com/juny/spacestory/global/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
@@ -88,9 +89,9 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated());
 
-    http.csrf(AbstractHttpConfigurer::disable)
-        .formLogin(AbstractHttpConfigurer::disable)
-        .httpBasic(AbstractHttpConfigurer::disable);
+    http.csrf(httpSecurityCsrfConfigurer -> CookieCsrfTokenRepository.withHttpOnlyFalse())
+      .formLogin(AbstractHttpConfigurer::disable)
+      .httpBasic(AbstractHttpConfigurer::disable);
 
     http.oauth2Login(
       (oauth2) ->
