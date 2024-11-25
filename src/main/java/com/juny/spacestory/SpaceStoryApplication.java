3a8db774b49113a8380785f27ccb6d60b477278d
ji-jjang <153375785+ji-jjang@users.noreply.github.com>
2024-11-25 16:21:56 +0900

    PR: 프로젝트 초기 설정 (#2)
    * feat: 프로젝트 초기 설정
    
    * feat: 환경별 설정파일 추가
    
    * feat: github-action CI 파이프라인 추가

---
+++ src/main/java/com/juny/spacestory/SpaceStoryApplication.java
@@ -0,0 +1,13 @@
+package com.juny.spacestory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceStoryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpaceStoryApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/juny/spacestory/SpaceStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juny.spacestory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceStoryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
