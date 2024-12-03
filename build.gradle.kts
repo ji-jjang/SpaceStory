34e1b23837128852c3bcba3121abc0621681e69f
jinyus <jinyhehe@gmail.com>
2024-12-03 14:25:40 +0900

    feat: spring-doc 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ repositories {
 }
 
 dependencies {
+    implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -32,6 +34,7 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
+    testRuntimeOnly("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
