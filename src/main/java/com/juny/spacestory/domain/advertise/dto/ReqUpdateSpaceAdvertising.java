f127a89899df471ae4956602788e624d8aee0c9f
jinyus <jinyhehe@gmail.com>
2024-12-13 20:33:35 +0900

    refactor: 광고 쿠폰 서비스 엔티티 반환

--- src/main/java/com/juny/spacestory/domain/advertise/dto/ReqUpdateSpaceAdvertising.java
+++ src/main/java/com/juny/spacestory/domain/advertise/dto/ReqUpdateSpaceAdvertising.java
@@ -1,7 +1,3 @@
 package com.juny.spacestory.domain.advertise.dto;
 
-public record ReqUpdateSpaceAdvertising(
-  Boolean isAdvertised
-) {
-
-}
+public record ReqUpdateSpaceAdvertising(Boolean isAdvertised) {}
--- src/test/java/com/juny/spacestory/service/CouponServiceTest.java
+++ src/test/java/com/juny/spacestory/service/CouponServiceTest.java
@@ -3,7 +3,6 @@ package com.juny.spacestory.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-import com.juny.spacestory.domain.advertise.dto.ResCreateAdvertiseCoupon;
 import com.juny.spacestory.domain.advertise.entity.AdvertiseCoupon;
 import com.juny.spacestory.domain.advertise.repository.AdvertiseCouponRepository;
 import com.juny.spacestory.domain.advertise.service.AdvertiseCouponService;
@@ -19,11 +18,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class CouponServiceTest {
 
-  @InjectMocks
-  private AdvertiseCouponService couponService;
+  @InjectMocks private AdvertiseCouponService couponService;
 
-  @Mock
-  private AdvertiseCouponRepository advertiseCouponRepository;
+  @Mock private AdvertiseCouponRepository advertiseCouponRepository;
 
   @Test
   @DisplayName("3개월 광고 쿠폰을 생성한다")
@@ -33,25 +30,20 @@ public class CouponServiceTest {
     LocalDate startDate = LocalDate.now();
     LocalDate endDate = startDate.plusMonths(3);
     int price = 40000;
-    ResCreateAdvertiseCoupon expected = new ResCreateAdvertiseCoupon(
-      -1L,
-      startDate,
-      endDate,
-      price,
-      -1L
-    );
+    AdvertiseCoupon expected =
+        AdvertiseCoupon.builder().startDate(startDate).endDate(endDate).price(price).build();
 
     when(advertiseCouponRepository.save(any(AdvertiseCoupon.class)))
-      .thenAnswer(invocation -> invocation.getArgument(0));
+        .thenAnswer(invocation -> invocation.getArgument(0));
 
     // when
-    ResCreateAdvertiseCoupon coupon = couponService.createAdvertiseCoupon(3);
+    AdvertiseCoupon coupon = couponService.createAdvertiseCoupon(3);
 
     // then
     assertThat(coupon).isNotNull();
-    assertThat(coupon.startDate()).isEqualTo(expected.startDate());
-    assertThat(coupon.endDate()).isEqualTo(expected.endDate());
-    assertThat(coupon.price()).isEqualTo(expected.price());
+    assertThat(coupon.getStartDate()).isEqualTo(expected.getStartDate());
+    assertThat(coupon.getEndDate()).isEqualTo(expected.getEndDate());
+    assertThat(coupon.getPrice()).isEqualTo(expected.getPrice());
   }
 
   @Test
@@ -59,20 +51,20 @@ public class CouponServiceTest {
   void attachCouponToSpace() {
 
     // given
-    AdvertiseCoupon coupon = AdvertiseCoupon.builder()
-      .id(-1L)
-      .startDate(LocalDate.now().minusMonths(4))
-      .endDate(LocalDate.now().minusMonths(1))
-      .price(-1)
-      .space(null)
-      .build();
+    AdvertiseCoupon coupon =
+        AdvertiseCoupon.builder()
+            .id(-1L)
+            .startDate(LocalDate.now().minusMonths(4))
+            .endDate(LocalDate.now().minusMonths(1))
+            .price(-1)
+            .space(null)
+            .build();
 
     when(advertiseCouponRepository.findById(any(Long.class))).thenReturn(Optional.of(coupon));
 
     // when & then
-    assertThatThrownBy(
-      () -> couponService.updateAdvertiseCoupon(-1L, -1L, true))
-      .isInstanceOf(RuntimeException.class)
-      .hasMessageContaining("쿠폰 유효기간이 만료되었습니다.");
+    assertThatThrownBy(() -> couponService.updateAdvertiseCoupon(-1L, -1L, true))
+        .isInstanceOf(RuntimeException.class)
+        .hasMessageContaining("쿠폰 유효기간이 만료되었습니다.");
   }
 }
